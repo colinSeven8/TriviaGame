5c4ac284c01af6a4bd633cdbe64f92e7fa43a09c
colinSeven8 <ccnorse80@gmail.com>
2019-09-15 13:35:01 -0600

    Wireframing html, added some css and started with the game objects in js.

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+// Colin Reesor
+// Trivia Game
+// Timer homework
+
+//Game objects
+let trivia = [
+    {
+        "question":   "Which hit song featured the following lyric: 'The love we share seems to go nowhere and I've lost my light.'",
+        "answers":    ["a", "b", "Tainted Love", "d"]
+    },
+    {
+        "question":   "The Magic Number was a 1989 hit for the band, De La Soul. Which album of theirs did it appear on?",
+        "answers":    ["a", "b", "c", "3 Feet High and Rising"]
+    },
+    {
+        "question":   "Which 80's CLash song, when re-released in 1991 went stright to number 1 in the UK?",
+        "answers":    ["a", "b", "Should I stay or Should I Go?", "d"]
+    },
+    {
+        "question":   "Which Duran Duran song opened with the laughter of the keyboardist's girlfriend?",
+        "answers":    ["Hungry Like the Wolf", "b", "c", "d"]
+    },
+    {
+        "question":   "How",
+        "answers":    ["a", "b", "c", "d"]
+    }
+    {
+        "question":   "How",
+        "answers":    ["a", "b", "c", "d"]
+    }
+    {
+        "question":   "How",
+        "answers":    ["a", "b", "c", "d"]
+    }
+    {
+        "question":   "How",
+        "answers":    ["a", "b", "c", "d"]
+    }
+    {
+        "question":   "How",
+        "answers":    ["a", "b", "c", "d"]
+    }
+]
+//Initialize the game
+init();
+
+function init() {
+
+
+}
